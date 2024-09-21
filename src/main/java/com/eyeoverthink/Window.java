9d5ddeafb80fb476d3c06fd0ea16e7f3a5241bb1
vaughnscott <eyeoverthink@gmail.com>
2024-09-21 11:52:08 -0700

    Now we have updated the file to open, and when we press the space bar, it will gradually go from white to black.

--- src/main/java/com/eyeoverthink/Window.java
+++ src/main/java/com/eyeoverthink/Window.java
@@ -11,22 +11,32 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 public class Window {
     private int width, height;
     private String title;
     private long glfwWindow;
-    float r, g, b, a;
-    private boolean fadeScreenToBlack = false;
+
+    private float r, g, b, a;
+    private boolean fadeToBlack = false;
 
     private static Window window = null;
 
     private Window() {
         this.width = 1920;
         this.height = 1080;
-        this.title = "eyeoverthink";
+        this.title = "Mario";
         r = 1;
-        g = 1;
         b = 1;
+        g = 1;
         a = 1;
     }
 
@@ -39,10 +49,18 @@ public class Window {
     }
 
     public void run() {
-        System.out.println("Hello this is LWJGL version : " + Version.getVersion() + "!");
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
         init();
         loop();
+
+        // Free the memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and the free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     public void init() {
@@ -66,6 +84,11 @@ public class Window {
             throw new IllegalStateException("Failed to create the GLFW window.");
         }
 
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
         // Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
         // Enable v-sync
@@ -87,8 +110,18 @@ public class Window {
             // Poll events
             glfwPollEvents();
 
-            glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            if (fadeToBlack) {
+                r = Math.max(r - 0.01f, 0);
+                g = Math.max(g - 0.01f, 0);
+                b = Math.max(b - 0.01f, 0);
+            }
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+                fadeToBlack = true;
+            }
 
             glfwSwapBuffers(glfwWindow);
         }
